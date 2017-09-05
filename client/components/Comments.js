a31ceb468dc05ef8dd3dfd49fd64c8a472997036
Rahul Krishnan <rk2211@gmail.com>
2017-09-05 00:15:05 -0400

    Single photo display working

---
+++ client/components/Comments.js
@@ -0,0 +1,14 @@
+/* jshint esversion: 6 */
+import React from 'react';
+
+const Comments = React.createClass({
+  render() {
+    return (
+      <div className="comment">
+        Hello, this is the comment section!
+      </div>
+    )
+  }
+});
+
+export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,13 +1,18 @@
 /* jshint esversion: 6 */
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const index = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[index];
     return(
       <div className='single-photo'>
-        I am the single photo
+        <Photo index={index} post={post} {...this.props} />
+        <Comments />
       </div>
-    )
+    );
   }
 });
 
