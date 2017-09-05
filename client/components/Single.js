37390ae7c3d72d5698154f5ab25561b10b5bd473
Rahul Krishnan <rk2211@gmail.com>
2017-09-04 23:42:12 -0400

    Commit with basic Reduxtagram structure

---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+/* jshint esversion: 6 */
+import React from 'react';
+
+const Single = React.createClass({
+  render() {
+    return(
+      <div className='single-photo'>
+        I am the single photo
+      </div>
+    )
+  }
+});
+
+export default Single;
